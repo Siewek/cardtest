16049120bac54156de812b786e08409683a1573f
Siewek <sieweliusz@gmail.com>
2021-10-23 15:45:42 +0200

    Adding Main Menu

--- cardtest/Game.cs
+++ cardtest/Game.cs
@@ -6,6 +6,52 @@ namespace cardtest
 {
     class Game
     {
+        public void menu()
+        {
+            Console.WriteLine("Main Menu");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Available Commands:");
+            Console.WriteLine("new - starts new game");
+            Console.WriteLine("htp - displays how to play");
+            Console.WriteLine("help - displays functions available in game (also available after starting a new game)");
+            Console.WriteLine("credits - displays credits");
+            Console.WriteLine("quit - quits the game");
+            Console.ResetColor();
+            Console.WriteLine();
+        }
 
+        public void help()
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("List of commands: ");
+            Console.WriteLine("hit - adds a card to your hand");
+            Console.WriteLine("fold - folds this round");
+            Console.WriteLine("showhands - shows the cards on the table");
+            Console.WriteLine("help - displays help (duh)");
+            Console.WriteLine("quit - quits the program");
+            Console.WriteLine("back - goes back to main menu (only available in game)");
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+
+        public void credits()
+        {
+            Console.WriteLine();
+            Console.Write("Made by");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" Michał Olesiewicz");
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+        public void htp()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Each player starts with one card unknown to the other player.");
+            Console.WriteLine("The goal of the game is to get the sum of your cards as close to 21 as possible");
+            Console.WriteLine("with Jack, Queens and Kings counting as 10s and Ace counting as either 1 or 11, depending on what would bring you closer to the desired 21");
+            Console.WriteLine("If any player's card sum exceeds 21, they lose automatically");
+            Console.WriteLine("Use the help command for more info about available commands");
+        }
     }
 }
--- cardtest/Program.cs
+++ cardtest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CardGame.Models;
 using cardtest;
+using System.Threading;
 namespace ConsoleApp4
 {
     class Program
@@ -10,153 +11,211 @@ namespace ConsoleApp4
             Deck deck = new Deck();
             Player player = new Player();
             Player player2 = new Player();
-
+            Game game = new Game();
             int whostarts = deck.whostarts();
-            bool foldflag = false,endturnflag = false, opfoldflag = false;
+            bool foldflag = false, endturnflag = false, opfoldflag = false, backflag=false;
             string command;
             deck.displaytitle();
 
-            while(true)//the entire round
+            while (true)
             {
-                Console.WriteLine();
-                deck.builddeck();
-                player.clearhand();
-                player2.clearhand();
-                foldflag = false;
-                endturnflag = false;
-                opfoldflag = false;
-                //clean start
-                Console.WriteLine("Starting hands");
-                player.hit();
-                player2.hit();
-                player2.hidefirstcard();
-                Console.ForegroundColor = ConsoleColor.Red;
-                player2.showhand();
-                Console.ForegroundColor = ConsoleColor.Green;
-                player.showhand();
-                Console.ResetColor();
-                while (true)//showdown
+                if (backflag == true)
                 {
-                    if (whostarts == 2)
-                    {
-                        Console.WriteLine("Opponents Move");
-                        if (player2.value <= 17)
+                    Console.Clear();
+                    deck.displaytitle();
+                }
+                Console.WriteLine();
+                game.menu();
+                command = Convert.ToString(Console.ReadLine());
+                backflag = false;
+                switch (command)
+                {
+                    case "new":
                         {
-                            Console.WriteLine("Opponent Hit");
-                           player2.hit();                          
-                            whostarts = 1;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Opponent folded");
-                            opfoldflag = true;
-                            whostarts = 1;
-                        }
-                    }
-                    if(whostarts == 1 && player2.value <22)
-                    {
-                        endturnflag = false;
-                        Console.WriteLine("Your Move");
-                        if (foldflag != true)
-                        {
-                            while (!endturnflag)
+                            Console.Clear();
+                            while (true)//the entire round
                             {
-                                command = Console.ReadLine();
-                                switch (command)
+                                Console.WriteLine();
+                                Console.WriteLine("Dealing hands:");
+                                Console.WriteLine();
+                                deck.builddeck();
+                                player.clearhand();
+                                player2.clearhand();
+                                foldflag = false;
+                                endturnflag = false;
+                                opfoldflag = false;
+                                //clean start
+                                Console.WriteLine("Starting hands");
+                                player.hit();
+                                player2.hit();
+                                player2.hidefirstcard();
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                player2.showhand();
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                player.showhand();
+                                Console.ResetColor();
+                                while (true)//showdown
                                 {
-                                    case "hit":
+                                    if (whostarts == 2 && backflag ==false)
+                                    {
+                                        Console.WriteLine("Opponents Move");
+                                        if (player2.value <= 17)
                                         {
-                                            endturnflag = true;
-                                            player.hit();
-                                            Console.WriteLine("You Hit");
-                                            Console.ForegroundColor = ConsoleColor.Green;
-                                            player.showhand();
-                                            Console.ResetColor();
-                                            continue;
-
+                                            Console.WriteLine("Opponent Hit");
+                                            player2.hit();
+                                            whostarts = 1;
                                         }
-
-                                    case "fold":
+                                        else
                                         {
-                                            Console.WriteLine("You Folded");
-                                            foldflag = true;
-                                            endturnflag = true;
-                                            continue;
+                                            Console.WriteLine("Opponent folded");
+                                            opfoldflag = true;
+                                            whostarts = 1;
                                         }
-                                    case "showhands":
+                                    }
+                                    if (whostarts == 1 && player2.value < 22 && backflag == false)
+                                    {
+                                        endturnflag = false;                                      
+                                        if (foldflag != true)
                                         {
-                                            Console.WriteLine("Opponents Hand");
+                                            Console.WriteLine("Your Move");
+                                            Console.WriteLine();
+                                            while (!endturnflag)
+                                            {
+                                                command = Console.ReadLine();
+                                                switch (command)
+                                                {
+                                                    case "hit":
+                                                        {
+                                                            endturnflag = true;
+                                                            player.hit();
+                                                            Console.WriteLine("You Hit");
+                                                            Console.ForegroundColor = ConsoleColor.Green;
+                                                            player.showhand();
+                                                            Console.ResetColor();
+                                                            continue;
+
+                                                        }
+
+                                                    case "fold":
+                                                        {
+                                                            Console.WriteLine("You Folded");
+                                                            foldflag = true;
+                                                            endturnflag = true;
+                                                            continue;
+                                                        }
+                                                    case "showhands":
+                                                        {
+                                                            Console.WriteLine("Opponents Hand");
+                                                            Console.ForegroundColor = ConsoleColor.Red;
+                                                            player2.showhand();
+                                                            Console.ResetColor();
+                                                            Console.WriteLine("Your Hand");
+                                                            Console.ForegroundColor = ConsoleColor.Green;
+                                                            player.showhand();
+                                                            Console.ResetColor();
+                                                            continue;
+                                                        }
+                                                    case "quit":
+                                                        {
+                                                            return;
+                                                        }
+                                                    case "help":
+                                                        {
+                                                            game.help();
+                                                            break;
+                                                        }
+                                                    case "back":
+                                                    {
+                                                            backflag = true;
+                                                            endturnflag = true;
+                                                            break;
+                                                        }
+                                                    default:
+                                                        {
+                                                            Console.WriteLine("Unknown Command");
+                                                            break;
+                                                        }
+                                                }
+                                            }
+                                        }
+                                    }
+                                    Console.WriteLine();
+                                    whostarts = 2;
+                                    //win conditions
+                                    if ((opfoldflag == true && foldflag == true && backflag == false) || player.value > 21 || player2.value > 21)
+                                    {
+                                        player2.showfirstcard();
+                                        if (player.value > player2.value && player.value <= 21 || player2.value > 21)
+                                        {
                                             Console.ForegroundColor = ConsoleColor.Red;
                                             player2.showhand();
-                                            Console.ResetColor();
-                                            Console.WriteLine("Your Hand");
+                                            Console.ForegroundColor = ConsoleColor.Green;
+                                            player.showhand();
+                                            Console.ResetColor(); ;
+                                            Console.WriteLine("You Win");
+                                        }
+                                        else if (player.value < player2.value && player2.value <= 21 || player.value > 21)
+                                        {
+                                            Console.ForegroundColor = ConsoleColor.Red;
+                                            player2.showhand();
                                             Console.ForegroundColor = ConsoleColor.Green;
                                             player.showhand();
                                             Console.ResetColor();
-                                            continue;
+                                            Console.WriteLine("You Lose");
                                         }
-                                    case "quit":
+                                        else
                                         {
-                                            return;
+                                            Console.ForegroundColor = ConsoleColor.Red;
+                                            player2.showhand();
+                                            Console.ForegroundColor = ConsoleColor.Green;
+                                            player.showhand();
+                                            Console.ResetColor();
+                                            Console.WriteLine("It's a tie");
                                         }
-                                    case "help":
-                                        {
-                                            Console.WriteLine("List of commands: ");
-                                            Console.WriteLine("hit - adds a card to your hand");
-                                            Console.WriteLine("fold - folds this round");
-                                            Console.WriteLine("showhands - shows the cards on the table");
-                                            Console.WriteLine("help - displays help (duh)");
-                                            Console.WriteLine("quit - quits the program");
-                                            break;
-                                        }
-                                    default: 
-                                        {
-                                            Console.WriteLine("Unknown Command");
-                                            break;
-                                        }
+                                        Console.WriteLine("New game starts in 10 seconds");
+                                        Thread.Sleep(10000);
+                                        Console.Clear();
+                                        break;
+                                    }
+                                    if (backflag == false)
+                                        continue;
+                                    else
+                                        break;
                                 }
+                                if (backflag == false)
+                                {
+                                    continue;
+                                }
+                                else
+                                    break;
                             }
+                            continue;
                         }
-                    }
-                    Console.WriteLine();
-                    whostarts = 2;
-                    //win conditions
-                    if ((opfoldflag == true && foldflag == true)||player.value >21 || player2.value >21 )
-                    {
-                        player2.showfirstcard();
-                        if (player.value > player2.value && player.value <=21 || player2.value > 21)
+                    case "help":
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            player2.showhand();
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            player.showhand();
-                            Console.ResetColor(); ;
-                            Console.WriteLine("You Win");
+                            game.help();
+                            break;
                         }
-                        else if (player.value < player2.value && player2.value <= 21 || player.value >21)
+                    case "credits":
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            player2.showhand();
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            player.showhand();
-                            Console.ResetColor();
-                            Console.WriteLine("You Lose");
+                            game.credits();
+                            break;
                         }
-                        else
+                    case "quit":
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            player2.showhand();
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            player.showhand();
-                            Console.ResetColor();
-                            Console.WriteLine("It's a tie");
+                            return;
                         }
-                        break;
-                    }
-                    continue;
+                    case "htp":
+                        {
+                            game.htp();
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Unknown Command " + command);
+                            break;
+                        }
                 }
-               
-                continue;
             }
         }
     }
